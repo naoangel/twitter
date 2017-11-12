caf8a072bb1703f0d8cacf99c6815086b6416508
naoangel <es.veneros@gmail.com>
2017-11-12 05:10:19 -0300

    text area donde debe

--- app.js
+++ app.js
@@ -1,58 +1,27 @@
+	var boton = document.getElemenntById('btnstart');
+	boton.addEventListener('click',function({
+	
+   var Tarea = document.getElementById('textbox').value;
+	//obtiene el contenido del textarea  
+   document.getElementById('textbox').value ='';
+   	//limpiar el textarea
+   var cont = document.getElementById('commentscontainer');
+	//definir el contenedor en una variable.
+	cont.appendChild('tarea');
+	//agregar el elemento a la variable.
 
-var comment = document.getElementById('msg').value;
 
-document.getElementById('msg').value=''; /*limpia el textarea*/
-
-var padre = document.getElemenntById('contenedor');
-var newComments = document.createElement('div');
-var parrafo = document.createElement('p');
-var nodo = document.createTextNode(comment);
-
-
-
-
-var boton = document.getElemenntById('btn');
-boton.addEventListener('click',function({
 })
 
 
+	(function(){
+  var msg = document.getElementsByClassName("msg")[0],
+      charLeftLabel = "char-left",
+      charLeft = document.getElementsByClassName(charLeftLabel)[0],
+      maxChar = 140,
+      maxCharWarn = 20;
 
-
-/*
-var comment -> mensaje de text area
-var conten -> se refiere al div 
-contenedor de  los futuros mensajes
-(padre)
-var btn -> botón 
-var 
-*/   
-
-function addComment(){
-//crear checkbox
-	var chck = document.createElement('input');
-	chck.type = 'checkbox';
-	//creamos icono de basura
-	var trash = document.createElement('i');
-	trash.classList.add('fa', 'fa-trash', 'trash');
-	//nodos de texto del textarea
-	var textNewComment = document.createTextNode(comments);
-
-	var contenedorElemento = document.createElement('p');
-	contenedorElemento.appendChild(textNewComment);
-	newComments.appendChild(chck);
-	newComments.appendChild(trash);
-	newComments.appendChild(heart);
-	newComments.appendChild(contenedorElemento);
-
-	cont.appendChild(newComments);
-}
-
-if(comment.length == 0 || comments == null) {
-		alert('Debes ingresar un mensaje');
-		return false;
-	}
-
-
+})();
 
 
 

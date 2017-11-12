7a565fff139ccdd70b3ebc4bea7f51a207ec7429
naoangel <es.veneros@gmail.com>
2017-11-12 17:12:28 -0300

    función posteo

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-	var boton = document.getElemenntById('btnstart');
+	var boton = document.getElemenntById('btn');
 	boton.addEventListener('click',function({
 	
-   var Tarea = document.getElementById('textbox').value;
-	//obtiene el contenido del textarea  
-   document.getElementById('textbox').value ='';
-   	//limpiar el textarea
-   var cont = document.getElementById('commentscontainer');
-	//definir el contenedor en una variable.
-	cont.appendChild('tarea');
-	//agregar el elemento a la variable.
 
+  //obtener elementos necesarios
+   var tweet = document.getElementById('msg').value;
+	//obtiene el contenido del textarea  
+   document.getElementById('msg').value ='';
+   	//limpiar el textarea
+   var cont = document.getElementById('postbox');
+	//definir el contenedor en una variable.
+	
+	
+
+  //crear elementos necesarios
+  var tweetBox = document.createElement('div');
+  //div que contiene el comentario
+  var elementTweet = document.createElement('p');
+  //Crear el elemento del comentario 
+
+  //crear nodo de texto
+   nodoTweet = document.createTextNode('tweet')
+
+  //ordenar elementos
+  elementTweet.appendChild('tweet');
+  //agrego el contenido de la caja de texto al elemento p
+  tweetBox.appendChild('elementTweet');
+
+  tweetBox.appendChild('nodotweet');
+  //agregando el comentario a su caja personal.
+  cont.appendChild('tweetBox');
+  //agregando la caja al contenedor de tweets.
+
+  
 
 })
 
 
 	(function(){
-  var msg = document.getElementsByClassName("msg")[0],
+  var msg = document.getElementsByClassName("msg"
+    |)[0],
       charLeftLabel = "char-left",
       charLeft = document.getElementsByClassName(charLeftLabel)[0],
       maxChar = 140,
